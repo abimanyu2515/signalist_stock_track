9991b34e20aa9b1ba830951f080e6497aed2bd6a
ABIMANYU_S <ctsssabimanyu.s@gmail.com>
2026-01-05 20:51:41 +0530

    header and layouts

---
+++ components/UserDropdown.tsx
@@ -0,0 +1,67 @@
+'use client'
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Button } from "./ui/button"
+import { useRouter } from "next/navigation"
+import NavItems from "./NavItems"
+
+const UserDropdown = () => {
+    const router = useRouter()
+
+    const handleSignOut = async () => {
+        router.push('/sign-in')
+    }
+
+    const user = { name: 'Abimanyu', email: 'abi@example.com' }
+
+  return (
+     <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="flex items-center gap-3 text-gray-400 hover:bg-yellow-500">
+            <Avatar className="h-8 w-8">
+                <AvatarImage src="https://lh3.googleusercontent.com/ogw/AF2bZygImN8fkcoO5C0QZTQJPf7yG4V8bqBJ4Cx0tKjgLcgEhQ=s64-c-mo" alt={user.name} />
+                <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
+                    {user.name[0]}
+                </AvatarFallback>
+            </Avatar>
+            <div className="hidden md:flex flex-col items-start">
+                <span className="text-base font-medium text-gray-400">
+                    {user.name}
+                </span>
+                <span className="font-normal">
+                    {user.email}
+                </span>
+            </div>
+        </Button>
+      </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-46" align="start">
+        
+        <DropdownMenuGroup className="sm:hidden">
+          <nav>
+            <NavItems />
+          </nav>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator className="sm:hidden" />
+          <DropdownMenuItem onClick={handleSignOut}>
+            Log out
+          </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
+
+export default UserDropdown
