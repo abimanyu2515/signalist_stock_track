679b8b72452be16f155c83664cc8e12e6a2bbd8b
ABIMANYU_S <ctsssabimanyu.s@gmail.com>
2026-01-07 10:55:26 +0530

    Implement database setup

---
+++ database/mongoose.ts
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+declare global {
+    var mongooseCache: {
+        conn: typeof mongoose | null;
+        promise: Promise<typeof mongoose> | null;
+    }
+}
+
+let cached = global.mongooseCache;
+
+if (!cached) {
+    cached = global.mongooseCache = { conn: null, promise: null }
+}
+
+export const connectToDb = async () => {
+    if (!MONGODB_URI) {
+        throw new Error ('DB setup incomplete. MONGODB_URI is missing.');
+    }
+
+    if (cached.conn) {
+        return cached.conn;
+    }
+
+    if (!cached.promise) {
+        cached.promise = mongoose.connect(MONGODB_URI, {bufferCommands: false})
+    }
+
+    try {
+        cached.conn = await cached.promise;
+    } catch (err) {
+        cached.promise = null;
+        throw err;
+    }
+
+    console.log(`Connected to database ${process.env.NODE_ENV} - ${MONGODB_URI}`);
+    
+    return cached.conn;
+}
